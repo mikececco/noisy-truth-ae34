8643870f34bc9f6a7bc88c6f6072190214b4beb1
Mike Cecconello <mikececco2000@gmail.com>
2024-08-20 22:03:44 +0200

    video: 58:59

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
  *
  * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
  * `Env` object can be regenerated with `npm run cf-typegen`.
- *
+ 
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
